b61172b67fad6556286a10dbba1793488e54cf52
josearmandojacq <josearmandocastilloquiala@Joses-MBP.fritz.box>
2018-01-05 10:12:18 +0100

    Adding the server

---
+++ client_src/src/components/Meetups.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+
+class Meetup extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Meetup</h1>
+            </div>
+        );
+    }
+}
+
+export default Meetup;
